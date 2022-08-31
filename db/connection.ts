aee4a58b3c5b478176cb2d46f2b4256f9b7e5748
Gerardo Cunya <vcunya@auna.pe>
2022-08-30 22:28:46 -0500

    example.env and use env variables for connection

--- db/connection.ts
+++ db/connection.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const db = new Sequelize('postgres://saleor:saleor@localhost/node');
+const db = new Sequelize(process.env.DATABASE_URL || '');
 
 export default db;
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('postgres://saleor:saleor@localhost/node');
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const db = new sequelize_1.Sequelize(process.env.DATABASE_URL || '');
 exports.default = db;
 //# sourceMappingURL=connection.js.map
